49bb1c1ddfdc6556b6cfb53fb235fa421715ff98
tubone <meitantei-conan@live.jp>
2019-03-25 00:32:47 +0900

    Update zabbix-netatmo.sh

--- zabbix-netatmo.sh
+++ zabbix-netatmo.sh
@@ -3,4 +3,4 @@
 MODULE=$1
 SENSOR=$2
 
-/bin/python /usr/lib/zabbix/externalscripts/netatmo-api-python/printAllLastDataJson | jq .$MODULE.$SENSOR
+/bin/python /usr/lib/zabbix/externalscripts/print_all_lastdata_Json.py | jq .$MODULE.$SENSOR
